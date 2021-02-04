9147439e7429dab7c761cc224ac070a15b341f29
chanoonna <chanoonna@gmail.com>
2021-02-04 21:05:44 +0000

    add: getAllresercations from database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -71,7 +71,15 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT *
+  FROM reservations
+  JOIN properties
+    ON properties.id = reservations.property_id
+  WHERE guest_id=$1
+  LIMIT $2;
+  `, [guest_id, limit])
+    .then(res => res.rows);
 };
 exports.getAllReservations = getAllReservations;
 
@@ -85,7 +93,8 @@ exports.getAllReservations = getAllReservations;
  */
 const getAllProperties = function(options, limit = 10) {
   return pool.query(`
-  SELECT * FROM properties
+  SELECT *
+  FROM properties
   LIMIT $1
   `, [limit])
     .then(res => res.rows);
